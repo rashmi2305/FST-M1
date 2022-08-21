1ae1096fc269f37219203e0affe58249bd1e7e6c
rashmi2305 <31200938+rashmi2305@users.noreply.github.com>
2022-08-21 23:20:32 +0530

    Add files via upload

---
+++ Python/Activities/HelloWorld.py
@@ -0,0 +1,7 @@
+print("Hello World")
+name = input("enter username: ")
+age = input("enter age: ")
+
+print(13//7)
+year = str( ( 2020 - int(age) ) + 100 )
+print( name + " will be 100 years old in the year " + year )
